d61520d6c098ee174149690a2c049ea1815ba80d
Nicolas Marini <nmarini44@gmail.com>
2019-05-03 19:20:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodIds = 0
 let customerIds = 0
 let mealIds = 0
-let deliverIds = 0
+let deliveryIds = 0
 
 class Neighborhood{
   constructor(name){
