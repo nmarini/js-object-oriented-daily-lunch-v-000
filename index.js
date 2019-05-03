11fbc3112b1e4145ffb02b6d08e42e6043aea501
Nicolas Marini <nmarini44@gmail.com>
2019-05-03 18:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ let deliverId = 0
 
 class Neighborhood{
   constructor(name){
+    this.id = ++neighborhoodId;
     this.name = name;
-    store.neighborhoods.push(this)
+    store.neighborhoods.push(this);
   }
   deliveries(){
     return
@@ -20,5 +21,61 @@ class Neighborhood{
 
   meals(){
     return
+  }
+}
+
+class Customer{
+  constructor(name, neighborhoodId){
+    this.id = ++customerId;
+    this.name = name;
+    this.neighborhoodId = neighborhoodId
+
+    store.customers.push(this)    
+  }
+
+  deliveries(){
+    return
+  }
+
+  meals(){
+    return
+  }
+
+  totalSpent(){
+    return
+  }
+}
+
+class Meal{
+  constructor(title, price){
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+
+    store.meal.push(this);
+  }
+
+  deliveries(){
+    return
+  }
+
+  customers(){
+    return
+  }
+
+  byPrice(){
+    return
+  }
+  
+}
+
+class Delivery{
+  constructor(mealId, neighborhoodId, customerId){
+    this.id = ++deliverId;
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+
+    store.deliveries.push(this);
   }
 }
