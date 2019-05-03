d4dc752f56a5967c01127b224614f6257dadc634
Nicolas Marini <nmarini44@gmail.com>
2019-05-03 18:34:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ let neighborhoodId = 0
 let customerId = 0
 let mealId = 0
 let deliverId = 0
+
+class Neighborhood{
+  constructor(name){
+    this.name = name;
+    store.neighborhoods.push(this)
+  }
+  deliveries(){
+    return
+  }
+
+  customers(){
+    return  
+  }
+
+  meals(){
+    return
+  }
+}
